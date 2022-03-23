02a009bef2d41fbb03626fe80d041d2fe4b802c9
amitchew <49229715+amitchew@users.noreply.github.com>
2022-03-23 11:27:45 +0300

    First Commit

---
+++ app.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 23 09:53:32 2022
+
+@author: __mitchew__
+"""
+
+import numpy as np
+import pandas as pd
+import streamlit as st
+from pandas_profiling import ProfileReport
+from streamlit_pandas_profiling import st_profile_report
+
+st.set_page_config(page_title="Sparta X", page_icon=None, layout="centered", initial_sidebar_state="auto")
+
+st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
+
+st.markdown("""
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #DCDCDC;">
+  <a class="navbar-brand" target="_blank">Sparta X</a>
+</nav>
+""", unsafe_allow_html=True)
+
+# Web App Title
+st.markdown('''
+# **EDA for Sparta X** 
+''')
+st.caption('''
+## **Explanatory Data Analysis** 
+
+''')
+
+# Upload CSV data
+with st.sidebar.header('Upload your CSV data'):
+    uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
+
+
+# Pandas Profiling Report
+if uploaded_file is not None:
+    @st.cache
+    def load_csv():
+        csv = pd.read_csv(uploaded_file)
+        return csv
+    df = load_csv()
+    pr = ProfileReport(df, explorative=True)
+    st.header('**Input DataFrame**')
+    st.write(df)
+    st.write('---')
+    st.header('**Sparta X Profiling Report**')
+    st_profile_report(pr)
+else:
+    st.info('Awaiting for CSV file to be uploaded.')
+
+
+    
+    
